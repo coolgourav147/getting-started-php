525213e8211b00712ae7813dfcd7e568f104db1b
coolgourav147 <coolgourav147@gmail.com>
2017-04-25 14:21:55 +0000

    wercker build time!

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $cities = array("Amsterdam", "San Francisco", "Berlin",
-                "New York123", "Tokyo", "London");
+                "New York", "Tokyo", "London");
 
 header('Content-Type: application/json');
 echo json_encode($cities, JSON_PRETTY_PRINT);
