b95261faf9e660babc5c7fb441f48e2a0b480307
coolgourav147 <coolgourav147@gmail.com>
2017-04-25 21:33:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $cities = array("Amsterdam", "San Francisco", "Berlin",
-                "New York", "Tokyo", "London");
+                "New York", "Tokyo", "London","Jaipur");
 
 header('Content-Type: application/json');
 echo json_encode($cities, JSON_PRETTY_PRINT);
